6b488ff8bfc38467a69eea68d656796beb363660
khunt.kk2 <khunt.kk2@gmail.com>
2025-05-31 09:28:28 +0000

    Removed PWA

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Vue Todo PWA',
-        short_name: 'TodoApp',
-        description: 'A Vue.js Todo PWA application',
-        theme_color: '#4285f4',
-        background_color: '#ffffff',
-        display: 'standalone',
-        orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
-})``
+  plugins: [vue()],
+  server: {
+    port: 3000,
+  },
+});
